a86078a56bc205d90f97601c0c71056329eccbc7
Arsham <Arshamalh@gmail.com>
2022-07-21 21:25:23 +0430

    Add: Facade

---
+++ structural/facade/account.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type account struct {
+	name string
+}
+
+func newAccount(accountName string) *account {
+	return &account{
+		name: accountName,
+	}
+}
+
+func (a *account) checkAccount(accountName string) error {
+	if a.name != accountName {
+		return fmt.Errorf("account Name is incorrect")
+	}
+	fmt.Println("Account Verified")
+	return nil
+}
---
+++ structural/facade/wallet.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type wallet struct {
+	balance int
+}
+
+func newWallet() *wallet {
+	return &wallet{
+		balance: 0,
+	}
+}
+
+func (w *wallet) creditBalance(amount int) {
+	w.balance += amount
+	fmt.Println("Wallet balance added successfully")
+}
+
+func (w *wallet) debitBalance(amount int) error {
+	if w.balance < amount {
+		return fmt.Errorf("balance is not sufficient")
+	}
+	fmt.Println("Wallet balance is Sufficient")
+	w.balance = w.balance - amount
+	return nil
+}
